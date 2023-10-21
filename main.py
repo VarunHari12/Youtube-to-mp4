fac49cfecc138937fe6edf95a215659c9cf66114
VarunHari12 <varunhari@outlook.com>
2023-10-20 22:29:26 -0700

    Added comments
    commented code

--- main.py
+++ main.py
@@ -4,21 +4,21 @@ import os
 
 link = "https://www.youtube.com/watch?v=3VRv8wv2dZM" # link to video
 
-pathV = "C:\\Users\\varun\\Documents\\GitHub\\Youtube-to-mp4\\Videos\\video" # whatever path you would like to choose
-pathA = "C:\\Users\\varun\\Documents\\GitHub\\Youtube-to-mp4\\Videos\\audio"
-path = "C:\\Users\\varun\\Documents\\GitHub\\Youtube-to-mp4\\Videos"
+pathV = "C:\\Users\\varun\\Documents\\GitHub\\Youtube-to-mp4\\Videos\\video" # This not the final video path, this is just a temporary folder
+pathA = "C:\\Users\\varun\\Documents\\GitHub\\Youtube-to-mp4\\Videos\\audio" # This not the final audio path, this is just a temporary folder
+path = "C:\\Users\\varun\\Documents\\GitHub\\Youtube-to-mp4\\Videos" # This is the final path of the video
 
-title = input("Enter a title: ")
+title = input("Enter a title: ") # title of final video
 
 try:
     
-    yt = YouTube(link)
+    yt = YouTube(link) # video object
     
 except:
     print("error")
     
-audio = yt.streams.filter(only_audio=True).first()   
-video = yt.streams.get_highest_resolution()
+audio = yt.streams.filter(only_audio=True).first() # audio clip
+video = yt.streams.get_highest_resolution() # video clip
 
 
 
@@ -26,14 +26,14 @@ video = yt.streams.get_highest_resolution()
 
 try:
     
-    video.download(pathV)
-    audio.download(output_path=pathA,filename=f"{video.title}.mp3")
+    video.download(pathV) # downloads video
+    audio.download(output_path=pathA,filename=f"{video.title}.mp3") # downloads audio
     
 except:
     print("Download Error")
 
-audio_clip = AudioFileClip("C:\\Users\\varun\\Documents\\GitHub\\Youtube-to-mp4\\Videos\\audio\\" + f"{video.title}.mp3")
-video_clip = VideoFileClip("C:\\Users\\varun\\Documents\\GitHub\\Youtube-to-mp4\\Videos\\video\\" + f"{video.title}.mp4")
+audio_clip = AudioFileClip("C:\\Users\\varun\\Documents\\GitHub\\Youtube-to-mp4\\Videos\\audio\\" + f"{video.title}.mp3") # path of audio
+video_clip = VideoFileClip("C:\\Users\\varun\\Documents\\GitHub\\Youtube-to-mp4\\Videos\\video\\" + f"{video.title}.mp4") # path of video 
 
-final_clip = video_clip.set_audio(audio_clip)
-final_clip.write_videofile("C:\\Users\\varun\\Documents\\GitHub\\Youtube-to-mp4\\Videos\\Final_Video\\" + title +".mp4")
+final_clip = video_clip.set_audio(audio_clip) # splices together video and audio clip for highest quality
+final_clip.write_videofile("C:\\Users\\varun\\Documents\\GitHub\\Youtube-to-mp4\\Videos\\Final_Video\\" + title +".mp4") # downloads final video
